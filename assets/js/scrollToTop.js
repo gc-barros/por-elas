2a573ff3ba39a96259889945e47c883fd8735f1c
Gabriel Barros <87779356+gc-barros@users.noreply.github.com>
2021-10-23 20:50:54 -0300

    Ajustes finais

--- assets/js/scrollToTop.js
+++ assets/js/scrollToTop.js
@@ -18,8 +18,8 @@ function scrollFunction() {
     // Esconde o botão após 2500ms, se o scroll parar
     scrollTimer = window.setTimeout("hideButton()", 2000);
 
-    // Quando o usuário scrollar a página 4800px para baixo, exibe o botão
-    if (document.body.scrollTop > 4800 || document.documentElement.scrollTop > 4800) {
+    // Quando o usuário scrollar a página 3200px para baixo, exibe o botão
+    if (document.body.scrollTop > 3200 || document.documentElement.scrollTop > 3200) {
         mybutton.style.display = "block";
        
     } else {
--- assets/js/viewportAdjust.js
+++ assets/js/viewportAdjust.js
@@ -4,9 +4,9 @@ function ajustarViewport () {
     // <meta id="vp" name="viewport" content="width=device-width, initial-scale=1.0">
 
     if (screen.width < 480) {
-        // Se a largura da tela for menor que 480px, irá setar a width da meta tag para 320.
+        // Se a largura da tela for menor que 480px, irá setar a width da meta tag para 340.
         var mvp = document.getElementById('vp');
-        mvp.setAttribute('content','width=320');
+        mvp.setAttribute('content','width=340');
     } else {
         var mvp = document.getElementById('vp');
         mvp.setAttribute('content','width=device-width, initial-scale=1.0');
