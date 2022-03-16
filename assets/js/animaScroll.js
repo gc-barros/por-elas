044aa6407c29488a2ac4fa894cfcefcde5aca08a
Gabriel Barros <super.gcb18@gmail.com>
2022-03-16 20:18:54 -0300

    Add animações ao scrollar a página

---
+++ assets/js/animaScroll.js
@@ -0,0 +1,29 @@
+function animaScrollInit() {
+  const target = document.querySelectorAll("[data-anima]");
+  const animationClass = "animate";
+
+  if (target.length) {
+    window.addEventListener("scroll", function () {
+      animaScroll();
+    });
+  }
+
+  function animaScroll() {
+    const windowMetade = window.innerHeight * 0.75;
+
+    target.forEach((element) => {
+      const elementTop = element.getBoundingClientRect().top;
+      const isElementVisible = elementTop - windowMetade < 0;
+
+      if (isElementVisible) {
+        element.classList.add(animationClass);
+      } else {
+        element.classList.remove(animationClass);
+      }
+    });
+  }
+
+  animaScroll();
+}
+
+export default animaScrollInit;
